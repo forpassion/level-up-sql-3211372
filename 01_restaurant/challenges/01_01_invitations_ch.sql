131cebe56c302599618f83e66648e85842684d7d
forpassion <double99check@gmail.com>
2023-10-12 21:38:05 -0700

    Update 01_01_invitations_ch.sql

--- 01_restaurant/challenges/01_01_invitations_ch.sql
+++ 01_restaurant/challenges/01_01_invitations_ch.sql
@@ -1,4 +1,6 @@
 -- Generate a list of customer information.
-
+select Firstname, lastname,  email 
+from customer
+order by lastname
 -- Show their first name, last name, and email address.
 -- Sort the list of results by last name.
